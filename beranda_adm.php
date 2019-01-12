d516da7c3f58aa52cbeb8339e95b4600eb24d5ad
FebinIN <46621159+FebinIN@users.noreply.github.com>
2019-01-12 16:33:24 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator SI Peminjaman Buku PERPUSTAKAAN CERIA</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="Images/perpus.PNG" width="300px" height="300px"></img></p>
+                          <p class="label-title" align="center"><strong>PERPUSTAKAAN CERIA</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Perustkaan CERIA.<br>
+                            Melalui halaman ini dapat dilakukan peminjaman buku, <br>
+                            dan juga pengelolaan data peminjaman buku.<br><br>
+                            <br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
